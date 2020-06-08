ceae82b361dfcf823b9486c66dd7afffdda864a9
小野優太 <bzbzbzstock@outlook.jp>
2020-06-08 15:31:24 +0000

    課題

---
+++ create.blade.php
@@ -0,0 +1,25 @@
+{{-- layouts/profile.blade.phpファイルを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュース作成画面</h1>
+        <h1>応用課題４　create</h1>
+    </body>
+</html>
+
+@endsection
